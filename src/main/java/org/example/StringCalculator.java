7efb8fe3dd81c3bdb8dcbf1c1b812bffe9782d61
Dipak Rekhate <dipakrekhate@humancloud.co.in>
2024-10-26 11:45:22 +0530

    to pass the test case that accept empty string that business logic doen here

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -3,6 +3,10 @@ package org.example;
 public class StringCalculator {
 
     public int add(String input) {
-        return 0;
+        if (input.isEmpty()) {
+            return 0;
+        } else {
+            throw new IllegalArgumentException("input not valid");
+        }
     }
 }
