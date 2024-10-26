ff46f055ee7b8fe7d9d96c08e7e553b048862466
Dipak Rekhate <dipakrekhate@humancloud.co.in>
2024-10-26 11:54:24 +0530

    business logic done to pass test case that handel length one input

--- src/main/java/org/example/StringCalculator.java
+++ src/main/java/org/example/StringCalculator.java
@@ -5,6 +5,8 @@ public class StringCalculator {
     public int add(String input) {
         if (input.isEmpty()) {
             return 0;
+        } else if (input.length() == 1) {
+            return Integer.parseInt(input);
         } else {
             throw new IllegalArgumentException("input not valid");
         }
